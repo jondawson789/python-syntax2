4d324270168d4c7bda1e24bb815a5bb3cbdb4949
jdawson <jonathan.dawson789@mgmail.com>
2022-01-14 00:34:02 -0800

    first commit

---
+++ count_up.py
@@ -0,0 +1,23 @@
+def count_up(start, stop):
+    while(start != stop):
+        print(start)
+        start = start + 1
+    print(start)   
+
+
+"""Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+    # YOUR CODE HERE 
+
+count_up(5, 7)        
